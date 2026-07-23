cef2b7804bbc7a46fd7d88af8e4e053248689a4e
BelugaLosos <andrew.kondr.2025@gmail.com>
2026-07-23 16:23:25 +0300

    done writer
    TODO: reader

--- PositronUnitySdk/PositronSdk/Assets/PositronSdk/Scripts/NetworkIoApi/PositronNetworkWriter.cs
+++ PositronUnitySdk/PositronSdk/Assets/PositronSdk/Scripts/NetworkIoApi/PositronNetworkWriter.cs
@@ -4,6 +4,7 @@ using System.Buffers.Binary;
 using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Runtime.CompilerServices;
+using System.Text;
 
 namespace Positron.NetworkIoAPI
 {
@@ -18,12 +19,17 @@ namespace Positron.NetworkIoAPI
         /// <summary>
         /// All data containing in this writer. use it for sending via network
         /// </summary>
-        public ReadOnlySpan<byte> Data => new ReadOnlySpan<byte>(_buffer).Slice(0, _pointerPosition);
+        public ReadOnlySpan<byte> Data => _buffer.AsSpan(0, _pointerPosition);
+
+        /// <summary>
+        /// All data in buffer as ReadOnlyMemory
+        /// </summary>
+        public ReadOnlyMemory<byte> Memory => _buffer.AsMemory(0, _pointerPosition);
 
         /// <summary>
         /// Creates new writer
         /// </summary>
-        /// <param name="complexDataSerializer">Serializer tha used for writing structs or classes into network</param>
+        /// <param name="complexDataSerializer">Serializer than is used for writing structs or classes into network</param>
         /// <param name="bufferSize">main buffer size in bytes. by default is 65536 (64 KB)</param>
         /// <param name="tempBufferSize">buffer for serializing complex data. by default is 16384 (16 KB)</param>
         public PositronNetworkWriter(IPositronSerializer complexDataSerializer, int bufferSize = 64 * 1024, int tempBufferSize = 16 * 1024)
@@ -34,7 +40,7 @@ namespace Positron.NetworkIoAPI
         }
 
         /// <summary>
-        /// Resets writter
+        /// Resets writer
         /// </summary>
         /// <param name="eraseDataFully">If setted TRUE runs loop than rewrites all remaining data with zeros</param>
         public void Clear(bool eraseDataFully = false)
@@ -52,107 +58,98 @@ namespace Positron.NetworkIoAPI
         /// Writes int64 (aliased in C# as long) to buffer
         /// </summary>
         /// <param name="toWrite">signed int64 value</param>
-        /// <exception cref="IndexOutOfRangeException">Occures when new serialized data larger than remaining size in writer`s buffer</exception>
-        public void WriteLong(long toWrite)
-        {
-            const int VAR_SIZE = 8;
-            CheckSizeAndThrow(VAR_SIZE);
-
-            Span<byte> bufferSpan = _buffer;
-            BinaryPrimitives.WriteInt64BigEndian(bufferSpan.Slice(_pointerPosition, VAR_SIZE), toWrite);
-
-            MovePtr(VAR_SIZE);
-        }
+        /// <exception cref="IndexOutOfRangeException">Occurs when new serialized data larger than remaining size in writer`s buffer</exception>
+        public void WriteLong(long toWrite) => BinaryPrimitives.WriteInt64BigEndian(Balloc(8), toWrite);
 
         /// <summary>
         /// Writes int32 to buffer
         /// </summary>
         /// <param name="toWrite">signed int32 value</param>
-        /// <exception cref="IndexOutOfRangeException">Occures when new serialized data larger than remaining size in writer`s buffer</exception>
-        public void WriteInt(int toWrite)
-        {
-            const int VAR_SIZE = 4;
-            CheckSizeAndThrow(VAR_SIZE);
-
-            Span<byte> bufferSpan = _buffer;
-            BinaryPrimitives.WriteInt32BigEndian(bufferSpan.Slice(_pointerPosition, VAR_SIZE), toWrite);
-
-            MovePtr(VAR_SIZE);
-        }
+        /// <exception cref="IndexOutOfRangeException">Occurs when new serialized data larger than remaining size in writer`s buffer</exception>
+        public void WriteInt(int toWrite) => BinaryPrimitives.WriteInt32BigEndian(Balloc(4), toWrite);
 
         /// <summary>
         /// Writes int16 (aliased in C# as short) to buffer
         /// </summary>
         /// <param name="toWrite">signed int16 value</param>
-        /// <exception cref="IndexOutOfRangeException">Occures when new serialized data larger than remaining size in writer`s buffer</exception>
-        public void WriteShort(short toWrite)
-        {
-            const int VAR_SIZE = 2;
-            CheckSizeAndThrow(VAR_SIZE);
-
-            Span<byte> bufferSpan = _buffer;
-            BinaryPrimitives.WriteInt16BigEndian(bufferSpan.Slice(_pointerPosition, VAR_SIZE), toWrite);
-
-            MovePtr(VAR_SIZE);
-        }
+        /// <exception cref="IndexOutOfRangeException">Occurs when new serialized data larger than remaining size in writer`s buffer</exception>
+        public void WriteShort(short toWrite) => BinaryPrimitives.WriteInt16BigEndian(Balloc(2), toWrite);
 
         /// <summary>
         /// Writes uint64 (aliased in C# as ulong) to buffer
         /// </summary>
         /// <param name="toWrite">signed uint64 value</param>
-        /// <exception cref="IndexOutOfRangeException">Occures when new serialized data larger than remaining size in writer`s buffer</exception>
-        public void WriteUlong(ulong toWrite)
-        {
-            const int VAR_SIZE = 8;
-            CheckSizeAndThrow(VAR_SIZE);
-
-            Span<byte> bufferSpan = _buffer;
-            BinaryPrimitives.WriteUInt64BigEndian(bufferSpan.Slice(_pointerPosition, VAR_SIZE), toWrite);
-
-            MovePtr(VAR_SIZE);
-        }
+        /// <exception cref="IndexOutOfRangeException">Occurs when new serialized data larger than remaining size in writer`s buffer</exception>
+        public void WriteUlong(ulong toWrite) => BinaryPrimitives.WriteUInt64BigEndian(Balloc(8), toWrite);
 
         /// <summary>
         /// Writes uint32 (aliased in C# as uint) to buffer
         /// </summary>
         /// <param name="toWrite">signed uint32 value</param>
-        /// <exception cref="IndexOutOfRangeException">Occures when new serialized data larger than remaining size in writer`s buffer</exception>
-        public void WriteUint(uint toWrite)
-        {
-            const int VAR_SIZE = 4;
-            CheckSizeAndThrow(VAR_SIZE);
-
-            Span<byte> bufferSpan = _buffer;
-            BinaryPrimitives.WriteUInt32BigEndian(bufferSpan.Slice(_pointerPosition, VAR_SIZE), toWrite);
-
-            MovePtr(VAR_SIZE);
-        }
+        /// <exception cref="IndexOutOfRangeException">Occurs when new serialized data larger than remaining size in writer`s buffer</exception>
+        public void WriteUint(uint toWrite) => BinaryPrimitives.WriteUInt32BigEndian(Balloc(4), toWrite);
 
         /// <summary>
         /// Writes uint16 (aliased in C# as ushort) to buffer
         /// </summary>
         /// <param name="toWrite">signed uint16 value</param>
-        /// <exception cref="IndexOutOfRangeException">Occures when new serialized data larger than remaining size in writer`s buffer</exception>
-        public void WriteUshort(ushort toWrite)
+        /// <exception cref="IndexOutOfRangeException">Occurs when new serialized data larger than remaining size in writer`s buffer</exception>
+        public void WriteUshort(ushort toWrite) => BinaryPrimitives.WriteUInt16BigEndian(Balloc(2), toWrite);
+
+        /// <summary>
+        /// Writes bool as byte (0 or 1) to buffer
+        /// </summary>
+        /// <param name="toWrite">bool value</param>
+        public void WriteBool(bool toWrite) => Balloc(1)[0] = (byte)(toWrite ? 1 : 0);
+
+        /// <summary>
+        /// Writes one byte into buffer
+        /// </summary>
+        /// <param name="toWrite">byte value</param>
+        public void WriteByte(byte toWrite) => Balloc(1)[0] = toWrite;
+
+        /// <summary>
+        /// Writes one signed byte into buffer
+        /// </summary>
+        /// <param name="toWrite">signed byte value</param>
+        public void WriteSbyte(sbyte toWrite) => Balloc(1)[0] = (byte)toWrite;
+
+        /// <summary>
+        /// Writes float into buffer
+        /// </summary>
+        /// <param name="toWrite">float (Single or float32) value</param>
+        public void WriteFloat(float toWrite) => WriteInt(BitConverter.SingleToInt32Bits(toWrite));
+
+        /// <summary>
+        /// Writes double into buffer
+        /// </summary>
+        /// <param name="toWrite">double (Double or float64) value</param>
+        public void WriteDouble(double toWrite) => WriteLong(BitConverter.DoubleToInt64Bits(toWrite));
+
+        /// <summary>
+        /// Copies raw binary data buffer into internal buffer
+        /// </summary>
+        /// <param name="data">binary span (can be passed as ordinary byte[] it automatically casts into ReadOnlySpan)</param>
+        public void WriteBytes(ReadOnlySpan<byte> data)
         {
-            const int VAR_SIZE = 2;
-            CheckSizeAndThrow(VAR_SIZE);
-
-            Span<byte> bufferSpan = _buffer;
-            BinaryPrimitives.WriteUInt16BigEndian(bufferSpan.Slice(_pointerPosition, VAR_SIZE), toWrite);
-
-            MovePtr(VAR_SIZE);
+            WriteInt(data.Length);
+            data.CopyTo(Balloc(data.Length));
         }
 
-        public void WriteBool(bool toWrite)
+        /// <summary>
+        /// writes given string into internal buffer as bytes with UTF8 encoding
+        /// </summary>
+        /// <param name="toWrite"></param>
+        public void WriteString(string toWrite)
         {
-            const int VAR_SIZE = 1;
-            CheckSizeAndThrow(VAR_SIZE);
+            if (toWrite == null)
+            {
+                throw new ArgumentNullException();
+            }
 
-            Span<byte> bufferSpan = _buffer;
-            bufferSpan.Slice(_pointerPosition, VAR_SIZE)[0] = (byte)(toWrite ? 1 : 0);
-
-            MovePtr(VAR_SIZE);
+            int size = Encoding.UTF8.GetByteCount(toWrite);
+            WriteInt(size);
+            Encoding.UTF8.GetBytes(toWrite, Balloc(size));
         }
 
         /// <summary>
@@ -162,31 +159,44 @@ namespace Positron.NetworkIoAPI
         /// </summary>
         /// <typeparam name="T">any ref type or ordinary struct</typeparam>
         /// <param name="complex">data instance or copy</param>
-        /// <exception cref="InternalBufferOverflowException">Occures when new serialized data larger than remaining size in writer`s buffer</exception>
-        /// <exception cref="InternalBufferOverflowException">Occures when can`t put data to buffer correctly because serialized representation larger than 16 KB</exception>
-        public void Write<T>(T complex)
+        /// <exception cref="InternalBufferOverflowException">Occurs when can`t put data to buffer (complex data intermediate buffer) correctly because serialized representation larger than setted size</exception>
+        /// <exception cref="InternalBufferOverflowException">Occurs when new serialized data larger than remaining size in writer`s buffer</exception>
+        public void WriteComplexObject<T>(T complex)
         {
-            const int KB16 = 16 * 1024; 
+            if (complex == null)
+            {
+                throw new ArgumentNullException();
+            }
 
-            Span<byte> tempBuffer = _tempComplexDataSerializeBuffer;
-            int bytesWriten = _complexDataSerializer.Serialize(complex, _tempComplexDataSerializeBuffer);
+            const int SIZE_HEADER = 2;
+            int bytesWritten;
 
-            if (_pointerPosition + 2 + bytesWriten > _buffer.Length)
+            try
+            {
+                bytesWritten = _complexDataSerializer.Serialize(complex, _tempComplexDataSerializeBuffer.AsSpan());
+            }
+            catch(Exception e) 
+            {
+                throw new InternalBufferOverflowException("Can`t write complex data because internal serialize buffer is overloaded. extend it at creation time. Serializer E:", e);
+            }
+
+            if (_pointerPosition + SIZE_HEADER + bytesWritten > _buffer.Length)
             {
                 throw new InternalBufferOverflowException("PositronNetworkWriter overloaded, please clear it!");
             }
 
-            if (bytesWriten > KB16)
-            {
-                throw new InternalBufferOverflowException("PositronNetworkWriter can`t put data to buffer correctly because serialized representation larger than 16 KB");
-            }
+            WriteInt(bytesWritten);
+            _tempComplexDataSerializeBuffer.AsSpan(0, bytesWritten).CopyTo(_buffer.AsSpan(_pointerPosition, bytesWritten));
+            _pointerPosition += bytesWritten;
+        }
 
-            WriteUshort((ushort)bytesWriten);
+        private Span<byte> Balloc(int size)
+        {
+            CheckSizeAndThrow(size);
+            Span<byte> bufferSpan = _buffer.AsSpan(_pointerPosition, size);
+            _pointerPosition += size;
 
-            Span<byte> bufferSpan = _buffer;
-            tempBuffer.CopyTo(bufferSpan.Slice(_pointerPosition, bytesWriten));
-
-            MovePtr(bytesWriten);
+            return bufferSpan;
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
@@ -200,11 +210,5 @@ namespace Positron.NetworkIoAPI
 
         [DoesNotReturn]
         private void ThrowErr() => throw new InternalBufferOverflowException("PositronNetworkWriter overloaded, please clear it!");
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private void MovePtr(int size)
-        {
-            _pointerPosition += size;
-        }
     }
 }
